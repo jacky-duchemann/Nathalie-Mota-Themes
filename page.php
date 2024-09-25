5d3fd4bd5703a35a773c6aca2d7bbbbbb6bcd5be
jacky-duchemann <jackyduchemann@hotmail.fr>
2024-09-25 09:57:21 +0400

    Projet finalisé

--- page.php
+++ page.php
@@ -6,12 +6,15 @@ if (have_posts()) :
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <!-- <h1 class="entry-title"><?php the_title(); ?></h1> -->
                 <div class="entry-meta">
                     <?php
-                    echo '<span class="posted-on">Publié en mode le ' . get_the_date() . '</span>';
-                    echo '<span class="author"> par ' . get_the_author() . '</span>';
+                    /*echo '<span class="posted-on">Publié en mode le ' . get_the_date() . '</span>';
+                    echo '<span class="author"> par ' . get_the_author() . '</span>';*/
                     ?>
+                </div>
+                <div class="entry-content">
+                    <?php the_content(); // Affiche le contenu de la page ?>
                 </div>
             </header><!-- .entry-header -->
 
